543b4f83724411c6f07dcdb0786fc4b6ee47bac6
CarlosBlancas969 <u20241a322@upc.edu.pe>
2026-06-16 18:55:33 -0500

    Update lesson-2-variables.java
    l2 commit

--- starter-files/lesson-2-variables.java
+++ starter-files/lesson-2-variables.java
@@ -1,1 +1,14 @@
 
+public class MisDatos {
+    public static void main(String[] args) {
+        String nombre = "Carlos Blancas";
+        int edad = 23;
+        double altura = 1.62;
+        boolean leGustaJava = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("¿Le gusta Java? " + leGustaJava);
+    }
+}
