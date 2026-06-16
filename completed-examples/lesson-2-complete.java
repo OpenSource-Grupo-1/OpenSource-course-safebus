2f9b88dc94ae4555be831d3f0d2f38a057506e64
CarlosBlancas969 <u20241a322@upc.edu.pe>
2026-06-16 18:56:21 -0500

    Update lesson-2-complete.java
    l2 commit 2

--- completed-examples/lesson-2-complete.java
+++ completed-examples/lesson-2-complete.java
@@ -1,1 +1,9 @@
 
+// Completa con TUS datos:
+String nombre = "___";
+int edad = ___;
+String ciudadFavorita = "___";
+
+System.out.println("Me llamo " + nombre);
+System.out.println("Tengo " + edad + " años");
+System.out.println("Mi ciudad favorita es " + ciudadFavorita);
